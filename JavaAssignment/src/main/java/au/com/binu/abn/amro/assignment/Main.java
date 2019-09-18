f247ed696285f9a9d5e1255d859f5b1687f285fa
Binu B Kurian <binubabykurian@gmail.com>
2019-09-19 00:07:09 +1000

    Updated .gitignore

--- JavaAssignment/src/main/java/au/com/binu/abn/amro/assignment/Main.java
+++ JavaAssignment/src/main/java/au/com/binu/abn/amro/assignment/Main.java
@@ -16,9 +16,13 @@ public class Main {
     public static void main(String[] args) {
         try {
             FileHandler fileHandler = new FileHandler();
+            LOG.log(Level.INFO,String.format("Initializing parsing of %s", INPUT_FILE_NAME));
             List<Transaction> transactions = Util.extractTransactions(fileHandler, INPUT_FILE_NAME);
+            LOG.log(Level.INFO,"Extraction of transactions completed");
             Map<String, Long> dailySummaryReportMap = Util.createDailySummaryReportMap(transactions);
+            LOG.log(Level.INFO,"Creation of daily summary report completed");
             fileHandler.writeToCsv(dailySummaryReportMap);
+            LOG.log(Level.INFO,String.format("Wrote daily summary report to %s", OUTPUT_FILE_NAME));
         }
         catch (Exception e) {
             LOG.log(Level.SEVERE,String.format("Failed to parse {} and generate {}", INPUT_FILE_NAME, OUTPUT_FILE_NAME),e);
